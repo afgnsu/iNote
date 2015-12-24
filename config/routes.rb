b89ce2817dc856cf69f7887dda0581da651d73e8
ryanyang1221 <ryanyang1221@gmail.com>
2015-12-24 16:03:02 +0000

    Add the relation between user and category and basic layout of user 'show'

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
   resources :users do
-    resources :categories  
+    resources :categories do
+      resources :notes  
+    end
   end
   
   # Example of regular route:
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224130115) do
+ActiveRecord::Schema.define(version: 20151224152900) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id"
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
