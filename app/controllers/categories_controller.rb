7acdfa20e0af94fe37ea4648e28e23f4538b0823
ryanyang1221 <ryanyang1221@gmail.com>
2015-12-24 16:37:22 +0000

    Be able to create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,22 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  
+  def new
+    @category = current_user.categories.build
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    if @user.categories.create(category_params)
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+  
+  private
+    
+    def category_params 
+      params.require(:category).permit(:name)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ActiveRecord::Base
   belongs_to :user
+  
+  def new
+    @category = current_user.categories.build
+  end
+  
+  def create
+  end
+  
 end
