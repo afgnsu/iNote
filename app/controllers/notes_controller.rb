8138dff53cab3aafa97a2dad2261a4c1fb14e23c
ryanyang1221 <ryanyang1221@gmail.com>
2015-12-28 07:26:24 +0000

    Finish new and create action of note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,21 @@
 class NotesController < ApplicationController
-  before_action :authenticate_user!    
+  before_action :authenticate_user!  
+  
+  def new
+    @category = Category.find(params[:category_id])
+    @note = @category.notes.build 
+  end  
+
+  def create
+    category = Category.find(params[:category_id])
+    if category.notes.create(note_params)
+      redirect_to user_category_path(current_user, category)
+    else
+      render :new
+    end
+  end 
+  
+  def note_params
+    params.require(:note).permit(:title, :description, :link)
+  end
 end
